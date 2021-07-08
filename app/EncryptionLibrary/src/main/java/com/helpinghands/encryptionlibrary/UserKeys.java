655346867383cde33a9c1cf2af81fbd0927d6024
Dhananjay <dhananjay.waghade.alpha@gmail.com>
2021-07-08 10:49:37 +0530

    Initial commit

---
+++ app/EncryptionLibrary/src/main/java/com/helpinghands/encryptionlibrary/UserKeys.java
@@ -0,0 +1,25 @@
+package com.helpinghands.encryptionlibrary;
+public class UserKeys {
+
+    private byte publicKey[];
+    private byte privateKey[];
+
+    public UserKeys() {
+    }
+
+    public byte[] getPublicKey() {
+        return publicKey;
+    }
+
+    public void setPublicKey(byte[] publicKey) {
+        this.publicKey = publicKey;
+    }
+
+    public byte[] getPrivateKey() {
+        return privateKey;
+    }
+
+    public void setPrivateKey(byte[] privateKey) {
+        this.privateKey = privateKey;
+    }
+}
